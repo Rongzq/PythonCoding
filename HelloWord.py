d83c611e7d23386921de3233718bd868df601a82
Jerry Rongzq <jerry.rongzq@gmail.com>
2017-02-22 15:42:08 +0800

    练习基本知识

--- HelloWord.py
+++ HelloWord.py
@@ -1,2 +1,20 @@
 x = 'Hello Word'
 print (x * 2)
+
+print ('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print (100+200)
+
+print('100 + 200 =' , 100+200)
+
+name = input()
+
+print('hello, ', name*5)
+
+#注释符
+#在python中如果语句是以冒号:结尾时, 缩进的语句则视为代码块.
+#需要注意的是在python中缩进一定使用4个空格作为缩进
+#在编程时, 注意需要将较长的代码拆分成若干的函数, 从而得到缩进较少的代码
+#Python是大小写敏感的, 需要注意.
+
+
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 studying Python
 
 Create repository
+
+##change history
+1. helloworld.py, 练习python基本知识
