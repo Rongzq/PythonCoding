0707731f31ffdd061e0f36add48339e1998a1d62
Rongzq <jerry.rongzq@gmail.com>
2017-03-02 14:57:05 +0800

    update about unpack function

--- ex13.py
+++ ex13.py
@@ -11,7 +11,7 @@ from sys import argv
 #script, first, second, 
 (script, first, second, third) = argv
 
-#print ("The script is called: ", script)
-#print ("Your first varible is: ", first)
-#print ("Your second varible is: ", second)
+print ("The script is called: ", script)
+print ("Your first varible is: ", first)
+print ("Your second varible is: ", second)
 print ("Your third varible is: ", third)
