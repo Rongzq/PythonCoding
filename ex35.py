3093c956f568d2037b311f652571f48d037227b6
Rongzq <jerry.rongzq@gmail.com>
2017-03-06 15:34:50 +0800

    some exercise for the HWPython

---
+++ ex35.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : Rongzq (jerry.rongzq@outlook.com)
+@Link    : https://github.com/Rongzq/PythonCoding
+@Date    : 2017/3/6 13:36
+@Version :
+@History : 
+
+
+"""
+
