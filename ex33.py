12c1ab7fdf28bb5e2b9b7c593c5e719980a3dd1d
Rongzq <jerry.rongzq@gmail.com>
2017-03-06 15:35:40 +0800

    some exercise

--- ex33.py
+++ ex33.py
@@ -3,9 +3,23 @@
 @Author  : Rongzq (jerry.rongzq@outlook.com)
 @Link    : https://github.com/Rongzq/PythonCoding
 @Date    : 2017/3/6 11:34
-@Version :
-@History : 
+@Version : 0.1
+@History : exercise of HWPython, while-loop
 
 
 """
 
+i = 0
+numbers = []
+
+while i < 6:
+    print("At the top i is %d" % i)
+    numbers.append(i)
+
+    i += 1
+    print("Numbers now: ", numbers)
+    print("At the bottom i is %d" % i)
+
+print("The numbers: ")
+for num in numbers:
+    print(num)
--- ex40.py
+++ ex40.py
@@ -4,8 +4,30 @@
 @Link    : https://github.com/Rongzq/PythonCoding
 @Date    : 2017/3/6 14:51
 @Version :
-@History : 
+@History : 字典练习
 
 
 """
 
+cities = {'CA': 'San Francisco', 'MI': 'Detroit', 'FL': 'Jacksonville'}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+def find_city(themap, state):
+    if state in themap:
+        return themap[state]
+    else:
+        return "Not found"
+
+cities['_find'] = find_city
+# 函数同样可以做变量, 将函数'find_city' 放在叫做'cities'的字典中, 并标记为'_find'
+
+while True:
+    print("State? (Enter to quite"),
+    state = input("> ")
+
+    if not state: break
+
+    city_found = cities['_find'](cities, state)
+    print(city_found)
